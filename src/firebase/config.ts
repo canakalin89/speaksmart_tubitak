4ecd6c079fce0a1cad048047d884f789f1553454
Can AKALIN <supertacos89@gmail.com>
2025-10-10 21:50:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2236383306-1852b",
+  "appId": "1:35434722775:web:f01326bcc7a5bea7468792",
+  "apiKey": "AIzaSyCRMU9W6e3UaVicMBC6WRcIpfb4IOkRhsI",
+  "authDomain": "studio-2236383306-1852b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "35434722775"
+};

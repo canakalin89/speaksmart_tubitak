39cdbe7605823377c20df4f5ba53783277ae2afb
Can AKALIN <supertacos89@gmail.com>
2025-10-14 19:31:12 +0000

    burada, kırmızı ile işaretledğim yerde sana attığım logo çıkacak. okul l

--- next.config.ts
+++ next.config.ts
@@ -52,12 +52,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
